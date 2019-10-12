b842ac821d069c518017adc92fe3650010acb1f3
Liu Hsin-Lin <alioth_liu@hotmail.com>
2019-10-12 16:54:50 -0700

    Completed the coding and tested the code

--- BitOrderConverter.cpp
+++ BitOrderConverter.cpp
@@ -1,24 +1,32 @@
-// BitOrderConverter.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Filename: BitOrderConverter.cpp
+// Description: To convert bit order of hex values, the rest of text is just copied
+// Author(s):  Elliot Liu
+// Date: Oct 12 2019
 
 #include <fstream>
 #include <iostream>
 
+const unsigned char hex[] = "0123456789ABCDEF";
+
 class bo_sm {
-	enum state
-	{
-		STANDBY,	//	waiting for the next input
-		CONTINUE,	//	waiting for the next hex input, caller should not write
-		COMPLETE	//	ready for the next input
-	};
 	unsigned char	data;	//	hold the binary data
-	std::ofstream& ofs;		
+	unsigned char	first_byte;
+	std::ofstream& ofs;	
+	enum  {READY, WAIT_FOR_X, WAIF_FOR_FIRST_BYTE, WAIT_FOR_SECOND_BYTE} stats;
 public:
-	bo_sm(std::ofstream& ofs_in): ofs(ofs_in);
-	put(unsigned char input);
+	bo_sm(std::ofstream& ofs_in) : ofs(ofs_in), stats(READY), data(0) {};
+	bool put(unsigned char input);
 };
 
+/*
+	main
+		open the input file and start the bit order control loop
 
+	Arguements:
+		argv[1]	IN
+			the input file name
+	Return:
+*/
 int main(int argc, char** argv)
 {
 	char* pfiledot;
@@ -42,18 +50,22 @@ int main(int argc, char** argv)
 	std::ifstream ifs(argv[1], std::ifstream::in);
 	std::ofstream ofs(target_file, std::ifstream::out);
 
+	bo_sm BitOrderConverter(ofs);
+
 	//	read char one by one
 	char c = ifs.get();
 
 	while (ifs.good()) {
 
 		//	enter the state machine
-		ofs.put(c);
-		if (!ofs.good()) {
-			std::cout << "Ouput file failed";
-			ofs.close();
-			ifs.close();
-			return EXIT_FAILURE;
+		if (BitOrderConverter.put(c)) {
+			ofs.put(c);
+			if (!ofs.good()) {
+				std::cout << "Ouput file failed";
+				ofs.close();
+				ifs.close();
+				return EXIT_FAILURE;
+			}
 		}
 		c = ifs.get();
 	}
@@ -67,7 +79,93 @@ int main(int argc, char** argv)
 	return EXIT_SUCCESS;
 }
 
-bo_sm::put(unsigned char input)
-{
+/*
+	bo_sm::put
+		get input byte by byte. If 0x is found, convert the next two bytes to binary and convert the bit order.
+		Write results back to the stream when conversion completes
 
+	Arguements:
+		input	IN
+			the input data in ASCII
+	Return:
+		bool
+			return true to tell the caller to copy the input data to output
+			return false to tell the caller to skip the copy action
+*/
+bool bo_sm::put(unsigned char input)
+{
+	bool ret = true;
+	switch(stats)
+	{
+	case READY:
+		if (input == '0')
+			stats = WAIT_FOR_X;
+		break;
+	case WAIT_FOR_X:
+		if (input == 'x')
+			stats = WAIF_FOR_FIRST_BYTE;
+		else
+			stats = READY;
+		break;
+	case WAIF_FOR_FIRST_BYTE:
+		stats = WAIT_FOR_SECOND_BYTE;
+		if (input >= '0' && input <= '9') {
+			data = input - '0';
+			ret = false;
+		}
+		else if (input >= 'A' && input <= 'F') {
+			data = input - 'A' + 10;
+			ret = false;
+		}
+		else if (input >= 'a' && input <= 'f') {
+			data = input - 'a' + 10;
+			ret = false;
+		}
+		else
+		{
+			stats = READY;
+			ret = true;
+		}
+		data <<= 4;
+		first_byte = input;
+		break;
+	case WAIT_FOR_SECOND_BYTE:
+		stats = READY;
+		if (input >= '0' && input <= '9') {
+			data += input - '0';
+			ret = false;
+		}
+		else if (input >= 'A' && input <= 'F') {
+			data += input - 'A' + 10;
+			ret = false;
+		}
+		else if (input >= 'a' && input <= 'f') {
+			data += input - 'a' + 10;
+			ret = false;
+		}
+		else
+		{	//	not valid hex value, put first byte back
+			ofs.put(first_byte);
+			ret = true;
+		}
+
+		if (ret == false) {
+			//	convert bit order
+			unsigned char new_val = 0;
+			for (unsigned char mask = 0x01;; mask <<=1) {
+				if (data & mask) {
+					new_val |= 0x01;
+				}
+				if (mask == 0x80)	break;
+				else {
+					new_val <<= 1;
+				}
+			}
+			//	convert data bits to hex value
+			ofs.put(hex[new_val >> 4]);
+			ofs.put(hex[new_val & 0x0F]);
+		}
+		break;
+	}
+	return ret;
 }
